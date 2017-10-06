283ba76b0c11fab789fc2c4c3f5669b608b3c758
kmart13 <kmart@kenitram.com>
2017-10-06 15:49:35 -0600

    React

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const react = require('./react/App.jsx');
 
 const hostname = 'localhost';
 const port = 8080;
@@ -6,7 +7,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  react.render()
 });
 
 server.listen(port, hostname, () => {
