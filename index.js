d42ee88c775ae1ac7f481abfa4d5e097ea8484cc
kmart13 <kmart@kenitram.com>
2017-10-06 15:18:39 -0600

    Configuration with Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Node app is running at http://${hostname}:${port}/`);
 });
